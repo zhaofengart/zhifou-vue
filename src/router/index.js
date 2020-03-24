cf51031b64102d17a24705a4d1001ed3ac8080b6
zhaofeng <2310571908@qq.com>
2020-03-24 16:39:21 +0800

    注册路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,12 +9,20 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: { title: '知否问答' }
     },
     {
       path: '/login',
       component: () => import('@/views/login'),
-      hidden: true
+      hidden: true,
+      meta: { title: '知否问答' }
+    },
+    {
+      path: '/register',
+      component: () => import('@/views/register'),
+      hidden: true,
+      meta: { title: '知否问答' }
     }
   ]
 })
