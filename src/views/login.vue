35d1c7d43a885b984db7a9566f58c00fbe430e4a
Robert0622 <1183605593@qq.com>
2020-03-25 13:35:46 +0800

    登录模块第四次，修复了上一个版本在IE浏览器中格式损坏的问题

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,42 +1,56 @@
 <template>
-  <div class = "register">
-    <el-row style="bottom: -50px; left: 380px;">
-      <el-col :xs="24">
-        <h1 style = "margin-left: 65px">华锐技术社区</h1>
-        <el-input style="width: 30%;
-                color: rgb(30, 159, 255);
-                background-color: rgb(0, 186, 189);
-                border-top-color: rgb(117, 26, 26);"
-                  v-model="loginForm.username"
-                  placeholder="username"
+  <div class="login">
+    <el-form ref="registerForm" :model="registerForm" :rules="registerRules" class="login-form" label-width="80px">
+      <h3 class="title">华锐技术社区</h3>
+      <el-form-item prop="username" label="用户名">
+        <el-input v-model="registerForm.username"
                   type="text"
-                  show-word-limit clearable>
+                  auto-complete="off"
+                  show-word-limit clearable
+        >
         </el-input>
-      </el-col>
-      <el-col :xs="24">
-        <el-input style="width: 30%;
-                color: rgb(1, 170, 237);
-                background-color: rgb(0, 186, 189);
-                margin-top: 34px;
-                margin-bottom: 34px;"
-                  v-model="loginForm.password"
-                  placeholder="password"
-                  type="text"
-                  show-word-limit clearable show-password>
+      </el-form-item>
+      <el-form-item prop="password" label="密码">
+        <el-input
+          v-model="registerForm.password"
+          type="password"
+          auto-complete="off"
+          show-word-limit clearable show-password
+        >
         </el-input>
-      </el-col>
-      <el-link @click="handelFindPassword" style="margin-top: -25px;margin-left: 250px" icon="el-icon-findpassword" type="primary">忘记密码？</el-link>
-      <el-col :xs="24">
-        <el-button @click="handellogin" style="background-color: rgb(1, 170, 237);
-                        color: rgb(57, 61, 73);">
-          登录</el-button>
-        <el-button @click.native.prevent="handelregister"
-                   style="margin-left: 190px;
-                   background-color: rgb(1, 170, 237);
-                   color: rgb(57, 61, 73);">
-          注册</el-button>
-      </el-col>
-    </el-row>
+      </el-form-item>
+      <el-link  @click="findPassWord"
+                style="margin-top: -25px;margin-left: 250px"
+                icon="el-icon-findpassword"
+                type="primary">忘记密码？ >
+      </el-link>
+      <el-form-item style="width:50%;">
+        <el-button-group>
+          <el-button
+            :loading="loading"
+            size="medium"
+            type="primary"
+            style="width:50%;"
+            @click.native.prevent="handlelogin"
+          >
+            <span v-if="!loading">登录</span>
+          </el-button>
+          <el-button
+            :loading="loading"
+            size="medium"
+            type="primary"
+            style="width:50%; margin-left: 160px;margin-top: -35.5px"
+            @click.native.prevent="handleRegister"
+          >
+            <span v-if="!loading">注册</span>
+          </el-button>
+        </el-button-group>
+      </el-form-item>
+    </el-form>
+    <!--  底部  -->
+    <div class="el-login-footer">
+      <span>Copyright © 2020 zhifou.huarui All Rights Reserved.</span>
+    </div>
   </div>
 </template>
 
@@ -44,36 +58,80 @@
   import { Message } from 'element-ui'
 
   export default {
-    name: "register",
+    name: "Register",
     data() {
       return {
-        loginForm: {
-          username: "",
-          password: "",
+        registerForm: {
+          username: '',
+          password: '',
         },
-        };
+        registerRules: {
+          username: [
+            { required: true, trigger: 'blur', message: '用户名不能为空' }
+          ],
+          password: [
+            { required: true, trigger: 'blur', message: '密码不能为空' }
+          ],
+        },
+        loading: false,
+      };
+    },
+    methods: {
+      handlelogin (){
+        this.$refs.registerForm.validate(valid => {
+          if (valid) {
+            Message.success({message: '你点击了登录按钮'})
+          }
+        })
       },
-    methods:{
-      handelregister (){
-        this.$router.push({path:'register'})
+      handleRegister () {
+        this.$router.push({path: 'register'})
       },
-      handelFindPassword (){
-        this.$router.push({path:'findPassword'})
-      },
-      handellogin (){
-        Message.success({message: '登录成功！'})
+      findPassWord (){
+        this.$router.push({path: 'findPassWord'})
       }
     }
-  };
+  }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-  .register {
+  .login {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100%;
     background-image: url("../assets/image/login-background.jpg");
+    background-color: #b8e5f8;
     background-size: cover;
+  }
+  .title {
+    margin: 0px auto 30px auto;
+    text-align: center;
+    color: #707070;
+  }
+
+  .login-form {
+    border-radius: 6px;
+    background: #ffffff;
+    width: 400px;
+    padding: 25px 25px 5px 25px;
+    .el-input {
+      height: 38px;
+      input {
+        height: 38px;
+      }
+    }
+  }
+  .el-login-footer {
+    height: 40px;
+    line-height: 40px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    text-align: center;
+    color: #fff;
+    font-family: Arial;
+    font-size: 12px;
+    letter-spacing: 1px;
   }
 </style>
