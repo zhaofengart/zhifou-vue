37b6a58b94fe7836160f3d08a3722742343f2ce1
zhaofeng <2310571908@qq.com>
2020-03-25 08:46:08 +0800

    修改注册页面背景图

--- src/views/register.vue
+++ src/views/register.vue
@@ -153,7 +153,8 @@ export default {
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/image/login-background.jpg");
+  background-image: url("../assets/image/login-background.png");
+  background-color: #b8e5f8;
   background-size: cover;
 }
 .title {
