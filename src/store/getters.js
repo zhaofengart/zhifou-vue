12f6fa7c5ff3ec3781247b2d06dd52fbc3b4f5e9
zhaofeng <2310571908@qq.com>
2020-04-03 16:01:38 +0800

    设置VUE_APP_BASE_API；删除用户的无用数据

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,6 @@
 const getters = {
   token: state => state.user.token,
   avatar: state => state.user.avatar,
-  name: state => state.user.name,
-  introduction: state => state.user.introduction,
-  roles: state => state.user.roles,
-  permissions: state => state.user.permissions
+  name: state => state.user.name
 }
 export default getters
